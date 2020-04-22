186d07edf23bcd1341444631d3173a0630143444
Nathan Isaac <nathan.isaac@wallawalla.edu>
2020-04-21 20:56:13 -0700

    Fix App tests
    Co-authored-by: Kevin Coleman <kevin.n.coleman@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {flushPromises} from '../tests/helpers.ts';
 
-it('renders without crashing', () => {
+it('renders without crashing', async () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  await flushPromises();
   ReactDOM.unmountComponentAtNode(div);
 });
